f4c58e1e75b799a708128dbd33eca1626634a1a4
Gökdeniz Çetin <gokdenizcetins@gmail.com>
2024-08-08 23:33:06 +0300

    create manifest ts and add favicon

---
+++ app/manifest.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+import SiteConfig from "@/config";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: SiteConfig.title,
+    short_name: "Next.js App",
+    description: SiteConfig.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      }
+    ],
+  };
+}
